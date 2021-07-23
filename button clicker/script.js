2fa22951052ec84b25f258be245e510dfbadb206
Yucong Ma <candicema2018@gmail.com>
2021-07-23 11:49:18 -0700

    finished assign

--- button clicker/script.js
+++ button clicker/script.js
@@ -6,3 +6,13 @@ function hide (element) {
 function changeText (element) {
   element.value = 'logout'
 }
+
+function addLike1 (element) {
+  element.value = '14 likes'
+  alert('Ninjia was liked')
+}
+
+function addLike2 (element) {
+  element.value = '38 likes'
+  alert('Ninjia was liked')
+}
