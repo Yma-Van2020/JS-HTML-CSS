5bfc70590d294a0f3aaa6c9f7e7b297ba9e9a72a
Yucong Ma <candicema2018@gmail.com>
2021-07-24 12:49:12 -0700

    finished

---
+++ liking likes/script.js
@@ -0,0 +1,22 @@
+
+var firstLabel = document.querySelector('.label1')
+var firstLikes = 9
+function likeIncrease1 () {
+  firstLikes++
+  firstLabel.innerText = firstLikes + " like(s)"
+}
+
+var secondLabel = document.querySelector('.label2')
+var secondLikes = 12
+function likeIncrease2 () {
+  secondLikes++
+  secondLabel.innerText = secondLikes + " like(s)"
+}
+
+
+var thirdLabel = document.querySelector('.label3')
+var thirdLikes = 9
+function likeIncrease3 () {
+  thirdLikes++
+  thirdLabel.innerText = thirdLikes + " like(s)"
+}
