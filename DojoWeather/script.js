0e968d45c08f7ec0a1a12899e66bf0e6fe9b65a2
Yucong Ma <candicema2018@gmail.com>
2021-07-25 14:32:58 -0700

    finished

--- DojoWeather/script.js
+++ DojoWeather/script.js
@@ -3,15 +3,18 @@ function hide(){
     cookieMsg.remove();
 }
 
-var high = document.querySelector(".left span")
-var low = document.querySelector(".right span")
+var high = document.querySelectorAll(".left span")
+var low = document.querySelectorAll(".right span")
 function convert(element){
-    if(element.value == "°F"){
-     high.innerText = (high.innerText * 9/5) + 32;
-     low.innerText = (low.innerText * 9/5) + 32;
-    } else {
-     high.innerText = (high.innerText - 32) * 5/9;
-     low.innerText = (low.innerText - 32) * 5/9;
-    }
-}
+
+    for(let j = 0; j < low.length;j++){
+        if(element.value == "°F"){
+         low[j].innerText = (low[j].innerText * 9/5) + 32;
+         high[j].innerText = (high[j].innerText * 9/5) + 32;
+        } else {
+         low[j].innerText = (low[j].innerText - 32) * 5/9;
+         high[j].innerText = (high[j].innerText - 32) * 5/9;
+        }
+
+}}
 
