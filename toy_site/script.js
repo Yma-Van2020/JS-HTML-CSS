6c63109c6c5fe3afdc31759385c836aa7b2f9fcd
Yucong Ma <candicema2018@gmail.com>
2021-07-29 09:56:36 -0700

    finished

---
+++ toy_site/script.js
@@ -0,0 +1,12 @@
+var cookieMsg = document.querySelector(".box5")
+function hide(){
+    cookieMsg.remove();
+}
+
+function change(elem){
+    elem.src = "images/assets/succulents-2.jpg"
+}
+
+function putback(elem){
+    elem.src = "images/assets/succulents-1.jpg"
+}
